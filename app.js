3aa7f8fe0fdea08ff6b39aa3bd86b52639c5d8c2
Mukhammadali Turdiboev <turdiboev0496@gmail.com>
2025-03-07 15:58:43 +0500

    Updated

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-let modal = document.querySelector(".burger-face")
+let modal = document.querySelector("#modal")
 let opener = document.querySelector("#opener")
 let closer = document.querySelector("#closer")
 
-console.log(closer);
 
 
 
-opener = addEventListener("click", () => {
-   console.log("Close button clicked");
-   modal.classList.add("show_modal")    
-
+opener.addEventListener("click", () => {
+   modal.classList.add("show_modal")       
     }
 );
 
 
-// closer = addEventListener("click", () => {
-    
-//     modal.classList.remove("show_modal")
-// });
+closer.addEventListener("click", () => {  
+    modal.classList.remove("show_modal")
+});
 
 modal.addEventListener("click", (e)=> {
     console.log(e.target);
@@ -25,3 +21,47 @@ modal.addEventListener("click", (e)=> {
         modal.classList.remove("show_modal")
     };
 })
+
+
+
+// let phrase = "Bugun boramizmi"
+
+// let a = {
+//     key: "yes"
+// }
+// let b = a;
+// b.key = "no"
+
+// let x = {
+//     ok: "ok"
+// }
+// let z = x;
+// let y = {
+//     ok: "ok"
+// }
+
+// console.log(y);
+
+
+// const obj = {
+//     firstName: "jhon",
+//     age: 30
+// }
+// obj.age = 40;
+
+// let first = {
+//     name: "Jhon",
+//     age: 30
+// }
+
+// for(let key in first) {
+//     clone
+// }
+
+// let f = {
+//     address:"Toshkent"
+
+// }
+// let clone = Object.assign(f, first);
+// clone.address = "Zomin";
+// console.log(f);
